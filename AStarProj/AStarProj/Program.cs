6890789ad07aa8912391b69c86d3f8eb73ddda89
hassan <hassanms@hassang.com>
2017-05-19 14:03:19 +0200

    first check in

---
+++ AStarProj/AStarProj/Program.cs
@@ -0,0 +1,12 @@
+﻿`using System;
+
+namespace AStarProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
